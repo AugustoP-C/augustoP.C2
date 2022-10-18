61012dcbdf70f5cf4d774de0dc5d3efe64a441a6
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-18 17:48:50 -0300

    projeto UEC

---
+++ src/UEC/luta.java
@@ -0,0 +1,48 @@
+package UEC;
+
+public class luta {
+    // atributos
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovação;
+    // metodos
+    public void marcarLuta() {
+
+    }
+    public void lutar() {
+
+    }
+    // geters e seterrs
+    public boolean isAprovação() {
+        return aprovação;
+    }
+
+    public void setAprovação(boolean aprovação) {
+        this.aprovação = aprovação;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public String getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(String desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public String getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(String desafiado) {
+        this.desafiado = desafiado;
+    }
+}
